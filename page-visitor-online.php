7e19d6cb1b57c15cee4947f0fce7d0e0e6cc6c4c
Alexander Permyakov <pedectrian@gmail.com>
2015-04-28 02:03:35 +0300

    Instance of class

--- page-visitor-online.php
+++ page-visitor-online.php
@@ -8,12 +8,14 @@
  */
 namespace Pedectrian;
 
-class PageVisitorsOnline {
-
+class PageVisitorsOnline
+{
+	const DB_VERSION = '0.0.1';
 
 	public function __construct()
 	{
-		add_action('init', array($this, 'init'));
+		add_action( 'init', array( $this, 'init' ) );
+		register_activation_hook( __FILE__, array($this, 'install') );
 	}
 
 	public function init()
@@ -25,6 +27,28 @@ class PageVisitorsOnline {
 
 			setcookie('pvo_hash', $user, time()+3600*24*100);
 		}
+	}
+
+	public function install()
+	{
+		global $wpdb;
+
+		$table_name = $wpdb->prefix . 'page_visitors_online';
+
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE $table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			visit_time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+			page_id tinytext NOT NULL,
+			user_hash text NOT NULL,
+			PRIMARY KEY id (id)
+		) $charset_collate;";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
+
+		add_option( 'pvo_db_version', PageVisitorsOnline::DB_VERSION );
 	}
 }
 
