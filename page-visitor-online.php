0a528d477e87f1713c333119522abc1427df4987
Alexander Permyakov <pedectrian@gmail.com>
2015-04-30 00:32:55 +0300

    Refactoring of online and daily visitors

--- page-visitor-online.php
+++ page-visitor-online.php
@@ -11,6 +11,8 @@ namespace Pedectrian;
 class PageVisitorsOnline
 {
 	const DB_VERSION = '0.0.1';
+	const VISITORS_ONLINE_DB = 'page_visitors_online';
+	const VISITORS_DAILY_DB = 'page_visitors_daily';
 
 	public $table_name;
 
@@ -33,33 +35,87 @@ class PageVisitorsOnline
 			setcookie('pvo_hash', $user, time()+3600*24*100);
 		}
 
-		if ($user) {
-			global $wpdb;
+		if ( $user ) {
 
-			$table_name = $wpdb->prefix . 'page_visitors_online';
-			$now = new \DateTime('now -3 minutes');
 
-			$wpdb->query(
-				$wpdb->prepare(
-					"
-                DELETE FROM $table_name
-				 	WHERE user_hash = %s OR
-				 	visit_date < '{$now->format('Y-m-d H:i:s')}'
-				",
-					$user
-				)
-			);
-			$thePost = get_page_by_path( $_SERVER['REQUEST_URI'] );
-			$wpdb->insert($table_name , array(
-					'visit_date' => date('Y-m-d H:i:s'),
-					'page_id' => $thePost->ID,
-					'user_hash' => $user
-				)
-			);
+
+
 		}
 	}
 
+	/**
+	 * Removes user's history and old (> 3min) page views
+	 * @param string $user
+	 */
+	protected function cleanOldLookers($user)
+	{
+		global $wpdb;
+		$now = new \DateTime('now -3 minutes');
+		$yesterday = new \DateTime('now');
+		$yesterday->setTime(0, 0, 0);
+		$onlineVisitorsTable = $wpdb->prefix . self::VISITORS_ONLINE_DB;
+		$daylyVisitorsTable = $wpdb->prefix . self::VISITORS_DAILY_DB;
+
+		$wpdb->query(
+			$wpdb->prepare(
+				"DELETE FROM {$onlineVisitorsTable}
+				 	WHERE user_hash = %s OR
+				 	visit_date < '{$now->format('Y-m-d H:i:s')}'
+				",
+				$user
+			)
+		);
+		$wpdb->query(
+			$wpdb->prepare(
+				"DELETE FROM {$daylyVisitorsTable}
+				 	WHERE user_hash = %s OR
+				 	visit_date < '{$yesterday->format('Y-m-d H:i:s')}'
+				",
+				$user
+			)
+		);
+	}
+
+	/**
+	 * Adds an online visitor and daily view
+	 * @param string $user
+	 */
+	protected function lookAtPage($user)
+	{
+		global $wpdb;
+		$onlineVisitorsTable = $wpdb->prefix . self::VISITORS_ONLINE_DB;
+		$daylyVisitorsTable = $wpdb->prefix . self::VISITORS_DAILY_DB;
+
+		$page = $_SERVER['REQUEST_URI'];
+
+		$wpdb->insert($onlineVisitorsTable , array(
+				'visit_date' => date('Y-m-d H:i:s'),
+				'page_id' => $page,
+				'user_hash' => $user
+			)
+		);
+
+		$wpdb->insert($daylyVisitorsTable , array(
+				'visit_date' => date('Y-m-d H:i:s'),
+				'page_id' => $page,
+				'user_hash' => $user
+			)
+		);
+	}
+
+	/**
+	 * Creates page_visitors_online table
+	 */
 	public function install()
+	{
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		$this->installVisitorsOnlineTable();
+		$this->installVisitorsDailyTable();
+
+		add_option( 'pvo_db_version', PageVisitorsOnline::DB_VERSION );
+	}
+
+	public function installVisitorsOnlineTable()
 	{
 		global $wpdb;
 
@@ -75,32 +131,46 @@ class PageVisitorsOnline
 			PRIMARY KEY id (id)
 		) $charset_collate;";
 
-		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 		dbDelta( $sql );
+	}
 
-		add_option( 'pvo_db_version', PageVisitorsOnline::DB_VERSION );
+	public function installVisitorsDailyTable()
+	{
+		global $wpdb;
+
+		$table_name = $wpdb->prefix . 'page_visitors_daily';
+
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE $table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			visit_date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+			page_id tinytext NOT NULL,
+			user_hash text NOT NULL,
+			PRIMARY KEY id (id)
+		) $charset_collate;";
+
+		dbDelta( $sql );
 	}
 
 	public function pageVisitorsOnlineShortcode()
 	{
 		global $wpdb;
-		$table_name = $wpdb->prefix . 'page_visitors_online';
+
 		$total_stats = $wpdb->prefix . 'kento_pvc';
-		$daily_stats = $wpdb->prefix . 'kento_pvc_info';
+		$onlineVisitorsTable = $wpdb->prefix . self::VISITORS_ONLINE_DB;
+		$daylyVisitorsTable = $wpdb->prefix . self::VISITORS_DAILY_DB;
 
 		global $post;
 
-		$total = $wpdb->query(
-			"SELECT count FROM $total_stats WHERE page_id = $post->ID LIMIT 1"
-		);
-
 		$total = $wpdb->get_var( "SELECT count FROM $total_stats WHERE page_id = $post->ID LIMIT 1" );
 		if (!$total) {$total = 0;}
-		$daily = $wpdb->get_var( "SELECT count FROM $daily_stats WHERE page_id = $post->ID LIMIT 1" );
+		$daily = $wpdb->get_var( "SELECT COUNT(id) FROM $daylyVisitorsTable WHERE page_id = $post->ID LIMIT 1" );
 		if (!$daily) {$daily = 0;}
 
-		$visits = $wpdb->get_var( "SELECT COUNT(id) FROM $table_name" );
-		return 'Просмотров: <b>за все время: </b>' . $total .'<b>, за сегодня: </b>' . $daily . '.<b> Читают сейчас: </b>' . $visits;
+		$page = $_SERVER['REQUEST_URI'];
+		$onlineVisitors = $wpdb->get_var( "SELECT COUNT(id) FROM $onlineVisitorsTable WHERE page_id = '{$page}'" );
+		return 'Просмотров: <b>за все время: </b>' . $total .'<b>, за сегодня: </b>' . $daily . '.<b> Читают сейчас: </b>' . $onlineVisitors;
 	}
 }
 
